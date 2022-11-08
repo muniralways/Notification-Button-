486d8eea1dadfc183d32ed16beac89f808ed463a
munir <shirajammunir2020@gmail.com>
2022-11-08 15:16:20 +0600

    Notification Button

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const notify = document.querySelector('.notify');
+const btn = document.querySelector('.btn');
+
+
+
+btn.addEventListener('click', active);
+
+function active (){
+    var add = Number (notify.getAttribute('data-count') || 0);
+    notify.setAttribute('data-count', add + 1);
+    if (add === 0){
+        notify.classList.add('add-numb');
+
+
+    }
+    false;
+
+}
